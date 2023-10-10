0142c5b0ecc705fd47ba69aba430c7ba313aa797
Christopher-Deo <chris@christopherdeo.com>
2023-10-10 06:49:44 -0500

    completed hangman game exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Welcome to the Love Calculator!")
-name1 = input("What is your name? \n")
-name2 = input("What is their name? \n")
+name1 = eval(input("What is your name? \n"))
+name2 = eval(input("What is their name? \n"))
 
 # convert names to lowercase
 combined_names = name1 + name2
